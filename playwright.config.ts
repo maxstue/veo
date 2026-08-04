df2b4c43619ec41d628eab54fd194b7b2940763e
maxstue <dev@justmax.xyz>
2026-08-04 16:57:19 +0200

    fix: allow for cold starts in browser tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: 1,
   reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : "list",
   expect: {
-    timeout: process.env.CI ? 15_000 : 5_000,
+    timeout: process.env.CI ? 30_000 : 5_000,
   },
   use: {
     baseURL: "http://localhost:5173",
